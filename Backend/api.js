9b558016a2788f642503386147a16a9eaf78e8b5
Manoj Naik <manoj.naik.4work@gmail.com>
2024-12-08 09:38:00 +0530

    Backend connection to mongoodb completed

--- Backend/api.js
+++ Backend/api.js
@@ -1,9 +1,33 @@
+const mongoose = require("mongoose");
 const express = require("express");
-const app = express();
+const dotenv = require("dotenv");
 const cors = require("cors");
-app.use(cors());
-app.use(express.json());
 
-app.listen(3000, function () {
-  console.log("server listening on port 3000");
+dotenv.config({ path: "./.env" }); 
+const app = express();
+
+// Middleware
+app.use(cors()); 
+app.use(express.json()); 
+
+// MongoDB Connection URI
+const dblink = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.xrrc8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Connect to MongoDB
+mongoose
+  .connect(dblink)
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.error("DB connection error:", err);
+  });
+
+  app.get("/api/test", (req, res) => {
+    res.json({ message: "Hello, world!" });
+  });
+
+// Start the server
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
 });
--- Frontend/src/components/Loading.jsx
+++ Frontend/src/components/Loading.jsx
@@ -1,12 +1,13 @@
 import React from "react";
- import "../main.css"
+import "../main.css";
 function Loading() {
-  return <>
-  <div className="flex items-center justify-center h-screen w-screen">
-
-  <div className="spinner "></div>
-  </div>
-  </>
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen w-screen">
+        <div className="spinner "></div>
+      </div>
+    </>
+  );
 }
 
 export default Loading;
