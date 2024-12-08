581bae655825d53f385cb5f7fd8c6c9e2422d3c3
Manoj Naik <manoj.naik.4work@gmail.com>
2024-12-08 08:49:56 +0530

    Implementation universal spinner completed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
+import Loading from "./components/Loading";
+
 
 // src/App.jsx
 function App() {
@@ -8,8 +10,9 @@ function App() {
     <>
       {/* <Home /> */}
       <Routes>
+        
         <Route path="/" element={<Home />}></Route>
-        <Route path="/login" element={<Login />}></Route>
+        <Route path="/login" element={<Loading />}></Route>
       </Routes>
     </>
   );
---
+++ Frontend/src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+ import "../main.css"
+function Loading() {
+  return <>
+  <div className="flex items-center justify-center h-screen w-screen">
+
+  <div className="spinner "></div>
+  </div>
+  </>
+}
+
+export default Loading;
