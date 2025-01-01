c203939eed2c89f54fe1ce0c89bf07b1c6821cb5
Manoj Naik <manoj.naik.4work@gmail.com>
2025-01-01 23:27:02 +0530

    Update the code and Center all contents in the Main component using Flexbox

--- Frontend/src/components/Dashboard.jsx
+++ Frontend/src/components/Dashboard.jsx
@@ -10,25 +10,32 @@ function Dashboard() {
 
   return (
     <>
-      <div className="sm:h-[300px] sm:w-[300px] lg:h-[400px] lg:w-[400px]  border-[2px] shadow-lg">
-        <img
-          src="src\components\image\logo2.jpg"
-          alt="boy image"
-          className="w-full"
-        />
-        <div className="h-[3rem] bg-gray-700 flex rounded-b-lg ">
-          <input
+      <div className="flex h-full  items-center justify-center">
+        <div className="sm:h-[300px] sm:w-[300px] lg:h-[400px] lg:w-[400px]  border-[2px] shadow-lg">
+          <img
+            src="src\components\image\logo2.jpg"
+            alt="boy image"
             className="w-full"
-            min="0"
-            max="100"
-            value={progressScore}
-            onChange={handleRangeChange}
-            type="range"
-            name=""
-            id=""
           />
+          <div className="h-[3rem] bg-gray-700 flex rounded-b-lg ">
+            <input
+              className="w-full"
+              min="0"
+              max="100"
+              value={progressScore}
+              onChange={handleRangeChange}
+              type="range"
+              name=""
+              id=""
+            />
+          </div>
+          <p className="text-center lg:text-lg">{progressScore}% Completed</p>
+          {progressScore === 100 && (
+            <h1 className="absolute top-[3rem] left-1/2 transform -translate-x-1/2 text-xl font-bold text-green-600">
+              You’re unstoppable! Keep chasing those big dreams!
+            </h1>
+          )}
         </div>
-        <p className="text-center lg:text-lg">{progressScore}% Completed</p>
       </div>
     </>
   );
--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -8,23 +8,16 @@ function Home() {
   const { progressScore } = useAuth();
   return (
     <div className="flex flex-col h-screen">
-      {/* Fixed Header */}
       <Header />
-
       <div className="flex flex-1 pt-[7rem]">
-        {/* Fixed Navigation */}
         <Nav />
-
         {/* Main Content */}
-        <div className="flex-1 flex items-center justify-center relative">
+        <div className="flex-1 relative">
           <Main />
-          {progressScore === 100 && (
-          <h1 className="absolute top-[3rem] left-1/2 transform -translate-x-1/2 text-xl font-bold text-green-600">
-               You’re unstoppable! Keep chasing those big dreams!
-            </h1>
-
-         )}
         </div>
+        {/* <div className="">
+          <Main />
+        </div> */}
       </div>
     </div>
   );
